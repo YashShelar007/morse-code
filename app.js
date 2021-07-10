bab3195b1a86a103cc4f5a15ea55060402d35dda
YashShelar007 <yshelar@asu.edu>
2021-07-10 16:12:27 -0700

    taking input and console

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log("Hello there I am working fine now!");
-username = prompt("give me your username");
-alert("I am here!! " + username);
+var buttonTranslate = document.querySelector("#button-translate");
+var textInput = document.querySelector("#txt-input");
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("The user has entered " + textInput.value);
+};
+
+buttonTranslate.addEventListener("click", clickHandler); 
