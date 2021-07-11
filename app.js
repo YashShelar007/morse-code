0b65fa83d82e978ec76cda7195f036ffe3560149
YashShelar007 <yshelar@asu.edu>
2021-07-10 18:10:07 -0700

    done without css

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var buttonTranslate = document.querySelector("#button-translate");
 var textInput = document.querySelector("#txt-input");
 var ouputText = document.querySelector("#output-txt");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return  (serverURL + "?" + "text=" + text);
+};
+
+
+
 function clickHandler() {
-    ouputText.innerText = "afswgvawfwaf " + textInput.value;
+    var inputText = textInput.value;
+
+    //server call
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => ouputText.innerText = json.contents.translated)
+    .catch(error => {console.log("error occured" + error); 
+    alert("something went wrong. Please try again later");}
+    );
+
 };
 
 buttonTranslate.addEventListener("click", clickHandler); 
