0cd510c75ccfd309de4cc8511af8ef19e52cf49a
YashShelar007 <yshelar@asu.edu>
2021-07-15 06:17:52 -0700

    updated styles

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var buttonTranslate = document.querySelector("#button-translate");
-var textInput = document.querySelector("#txt-input");
+var textInput = document.querySelector("#input-txt");
 var ouputText = document.querySelector("#output-txt");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/morse.json";
 
 function getTranslationURL(text) {
     return  (serverURL + "?" + "text=" + text);
 };
-
-
 
 function clickHandler() {
     var inputText = textInput.value;
@@ -16,11 +14,11 @@ function clickHandler() {
     //server call
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => ouputText.innerText = json.contents.translated)
+    .then(json => ouputText.innerHTML = json.contents.translated)
     .catch(error => {console.log("error occured" + error); 
     alert("something went wrong. Please try again later");}
     );
-
+    
 };
 
 buttonTranslate.addEventListener("click", clickHandler); 
